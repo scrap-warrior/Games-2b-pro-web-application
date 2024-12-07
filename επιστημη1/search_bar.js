d19413c8ecbc896dc8ad35dea9587451ee2e21a0
scrap-warrior <groditis2@gmail.com>
2024-12-07 23:02:57 +0200

    scroll_detection

--- επιστημη1/search_bar.js
+++ επιστημη1/search_bar.js
@@ -1,20 +1,25 @@
+
 function search_games() {
-    let input = document.getElementById('searchbar').value.toLowerCase();
-    let games = document.getElementsByClassName('Games');
-  
-    if (input.length == 0) {
-      for (let i = 0; i < games.length; i++) {
+  let input = document.getElementById('searchbar').value.toLowerCase();
+  let games = document.getElementsByClassName('Games');
+
+  if (input.length == 0) {
+    for (let i = 0; i < games.length; i++) {
+      games[i].style.display = 'none';
+    }
+  } else {
+    for (let i = 0; i < games.length; i++) {
+      if (games[i].innerHTML.toLowerCase().includes(input)) {
+        games[i].style.display = 'block'; // Ή block, αν προτιμάς
+      } else {
         games[i].style.display = 'none';
-      }
-    } else {
-      for (let i = 0; i < games.length; i++) {
-        if (games[i].innerHTML.toLowerCase().includes(input)) {
-          games[i].style.display = 'block'; // Ή block, αν προτιμάς
-        } else {
-          games[i].style.display = 'none';
-        }
       }
     }
   }
+}
   
-   
+window.addEventListener('scroll', function(event){
+  let scrollPosition = window.scrollY;
+  
+  console.log(scrollPosition);
+});
