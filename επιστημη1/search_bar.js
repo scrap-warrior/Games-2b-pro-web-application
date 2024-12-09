9d75d44f18e00f9ee23dc3d8806d62ebcd33395a
scrap-warrior <groditis2@gmail.com>
2024-12-09 18:32:02 +0200

    searchbar fixed(html to js)

--- επιστημη1/search_bar.js
+++ επιστημη1/search_bar.js
@@ -1,25 +1,35 @@
-
 function search_games() {
   let input = document.getElementById('searchbar').value.toLowerCase();
   let games = document.getElementsByClassName('Games');
-
+  
   if (input.length == 0) {
     for (let i = 0; i < games.length; i++) {
-      games[i].style.display = 'none';
+      games[i].style.display = 'none'; 
     }
   } else {
+    let visibleCount = 0; 
+    
     for (let i = 0; i < games.length; i++) {
       if (games[i].innerHTML.toLowerCase().includes(input)) {
-        games[i].style.display = 'block'; // Ή block, αν προτιμάς
+        if (visibleCount < 3) { 
+          games[i].style.display = 'block'; 
+          visibleCount++; 
+        } else {
+          games[i].style.display = 'none'; 
+        }
       } else {
         games[i].style.display = 'none';
       }
     }
   }
+
+
+
 }
+
+
+
+
   
-window.addEventListener('scroll', function(event){
-  let scrollPosition = window.scrollY;
   
-  console.log(scrollPosition);
-});
+   
