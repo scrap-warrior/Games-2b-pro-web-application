9fab9568406d53acce5ec9dccf259379ebfa6e14
scrap-warrior <groditis2@gmail.com>
2024-12-09 20:56:01 +0200

    json working

--- επιστημη1/search_bar.js
+++ επιστημη1/search_bar.js
@@ -1,3 +1,9 @@
+var gameData = ['{"game":'
++ '{"name": "Call of Duty", "imgs":["callOfDuty/img1.jpg","callOfDuty/img2.jpg" ,"callOfDuty/img3.jpg"],'
++' "description":"Call of Duty is a military first-person shooter video game series and media franchise published by Activision,'
++' starting in 2003. The games were first developed by Infinity Ward, then by Treyarch and Sledgehammer Games. Several spin-off and handheld games were made by other developers.'
++' The most recent, Call of Duty: Black Ops 6, was released on October 25, 2024."}}'];
+
 function search_games() {
   let input = document.getElementById('searchbar').value.toLowerCase();
   let games = document.getElementsByClassName('Games');
@@ -28,8 +34,7 @@ function search_games() {
 }
 
 
-
-
-  
-  
-   
+function loadGame(gameID){
+  var gameInfo = JSON.parse(gameData[gameID])
+  console.log(gameInfo)
+}
